0c0aeff04c71a7274d81b5eff15efad4c7198356
DevAnxllty <94204715+DevAnxllty@users.noreply.github.com>
2022-12-18 09:56:18 -0700

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,8 +1,25 @@
-local key = loadstring(game:HttpGet("https://raw.githubusercontent.com/HoyoGey/My-Lua-Project/main/UiLibs/KeySysUiLib.lua"))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
 
-key:MakeUI({
-  Title = "Azul Hub",
-  KeyLinkName = "Azul",
-  DiscordID = "ROBLOX",
-  ScriptLink = "https://google.com"
-})
+local is_key_present = isfile("AzulHubKey.txt");
+if is_key_present == true then
+    local kery = readfile("AzulHubKey.txt");
+    local onl_key = game:HttpGet("https://1.kelprepl.repl.co/verify/Azul?verify_key=" .. kery)
+    if kery == onl_key then
+        print("Executed script!");
+        lib:MakeNotification({
+            Name = "Successful!",
+            Content = "Key Correct, Load script",
+            Time = 5
+        })
+-- full script here:)
+    end
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HoyoGey/CoconutHub/main/Main.lua"))()
+    --wrong key
+    lib:MakeNotification({
+        Name = "Warning!",
+        Content = "Wrong Key Pls Try Again",
+        Image = "rbxassetid://10894994115",
+        Time = 5
+    })
+end
